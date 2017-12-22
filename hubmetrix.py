a70bb07d320d7e103431a24393556bd79d620429
nprutan <nprutan@gmail.com>
2017-12-21 22:31:01 -0700

    Initial commit

---
+++ hubmetrix.py
@@ -0,0 +1,269 @@
+from bigcommerce.api import BigcommerceApi
+from flask import Flask, session, redirect, url_for, request, render_template, Response
+from hubspot_utils import *
+from dynamodb_utils import *
+import chargebee
+import os
+import json
+
+app = Flask(__name__)
+
+# TODO: add to zappa envs
+app.config['DEBUG'] = os.getenv('DEBUG', True)
+app.config['LISTEN_HOST'] = os.getenv('LISTEN_HOST', '0.0.0.0')
+app.config['LISTEN_PORT'] = int(os.getenv('LISTEN_PORT', '5000'))
+app.config['APP_URL'] = os.getenv('APP_URL', 'https://9kr5377xjf.execute-api.us-west-1.amazonaws.com')  # must be https to avoid browser issues
+app.config['BC_CLIENT_ID'] = os.getenv('BC_CLIENT_ID', '5sdgtpwp82lxz5elfh5c5e2uvjzd07g')
+app.config['BC_CLIENT_SECRET'] = os.getenv('BC_CLIENT_SECRET', 'rua2g02c6t6b6eloudlne400jnmod4z')
+app.config['SESSION_SECRET'] = os.getenv('SESSION_SECRET', os.urandom(64))
+app.config['HS_REDIRECT_URI'] = 'https://9kr5377xjf.execute-api.us-west-1.amazonaws.com/dev/hsauth'
+app.config['HS_CLIENT_ID'] = 'f0e25e19-d1f3-48fe-93fc-59d744e2326e'
+app.config['HS_CLIENT_SECRET'] = '74c80cfa-9bed-4770-8e31-a581df04a181'
+
+app.secret_key = app.config['SESSION_SECRET']
+
+#
+# Error handling and helpers
+#
+def error_info(e):
+    content = ""
+    try:  # it's probably a HttpException, if you're using the bigcommerce client
+        content += str(e.headers) + "<br>" + str(e.content) + "<br>"
+        req = e.response.request
+        content += "<br>Request:<br>" + req.url + "<br>" + str(req.headers) + "<br>" + str(req.body)
+    except AttributeError as e:  # not a HttpException
+        content += "<br><br> (This page threw an exception: {})".format(str(e))
+    return content
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    content = "Internal Server Error: " + str(e) + "<br>"
+    content += error_info(e)
+    return content, 500
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    content = "Bad Request: " + str(e) + "<br>"
+    content += error_info(e)
+    return content, 400
+
+
+# Helper for template rendering
+def render(template, context):
+    return render_template(template, **context)
+
+
+def get_bc_client_id():
+    return app.config['BC_CLIENT_ID']
+
+
+def get_bc_client_secret():
+    return app.config['BC_CLIENT_SECRET']
+
+
+def get_hs_client_id():
+    return app.config['HS_CLIENT_ID']
+
+
+def get_hs_client_secret():
+    return app.config['HS_CLIENT_SECRET']
+
+
+def get_hs_redir_uri():
+    return app.config['HS_REDIRECT_URI']
+
+
+def construct_hubspot_auth_url():
+    client_id = get_hs_client_id()
+    redir_uri = get_hs_redir_uri()
+
+    return 'https://app.hubspot.com/oauth/authorize?client_id={}&scope=contacts%20automation%20timeline&redirect_uri={}'.format(
+        client_id, redir_uri)
+
+
+def construct_chargebee_signup_url(sess):
+    chargebee.configure("test_RqcuUOam2qv17jcusG0eiypcuZlaiQH7Zc", "composedcloud-test")
+
+    try:
+        email = sess['storeuserid']
+    except (KeyError, QueryError):
+        return "User not logged in! Please go back and click on app icon in admin panel.", 401
+
+    result = chargebee.HostedPage.checkout_new({
+        "subscription": {
+            "plan_id": "hubmetrix-base-plan"
+        },
+        "customer": {
+            "email": email
+        },
+        "redirect_url": app.config['APP_URL'] + url_for('payment_success'),
+        "embed": True
+    })
+    return result.hosted_page.values['url']
+
+
+@app.route('/')
+def index():
+    # app_user = get_query_first_result(AppUser, session['storeuserid'])
+    # if not app_user:
+    #     return "Not logged in!", 401
+    #
+    # # Construct api client
+    # client = BigcommerceApi(client_id=client_id(),
+    #                         store_hash=app_user.bc_store_hash,
+    #                         access_token=app_user.bc_access_token)
+    #
+    # # Fetch a few products
+    # orders = client.Orders.all(limit=10)
+    #
+    # # Render page
+    # context = dict()
+    # context['products'] = orders
+    # context['user'] = app_user.email
+    # context['store'] = app_user.bc_store_hash
+    # context['client_id'] = client_id()
+    # context['api_url'] = client.connection.host
+
+    hs_auth_url = construct_hubspot_auth_url()
+
+    return render_template('index.html')
+
+
+@app.route('/bigcommerce/callback')
+def auth_callback():
+    # Put together params for token request
+    code = request.args['code']
+    context = request.args['context']
+    scope = request.args['scope']
+    store_hash = context.split('/')[1]
+    redirect_uri = app.config['APP_URL'] + url_for('auth_callback')
+
+    # Fetch a permanent oauth token. This will throw an exception on error,
+    # which will get caught by our error handler above.
+    client = BigcommerceApi(client_id=get_bc_client_id(), store_hash=store_hash)
+    token = client.oauth_fetch_token(get_bc_client_secret(), code, context, scope, redirect_uri)
+    bc_id = token['user']['id']
+    email = token['user']['email']
+    access_token = token['access_token']
+
+    app_user = get_query_first_result(AppUser, email)
+    if not app_user:
+        app_user = AppUser(email, bc_id, bc_access_token=access_token, bc_scope=scope, bc_store_hash=store_hash)
+
+    app_user.save()
+    # Log user in and redirect to app home
+    session['storeuserid'] = app_user.email
+    return redirect(app.config['APP_URL'] + url_for('get_started'))
+
+
+@app.route('/bigcommerce/load')
+def load():
+    # Decode and verify payload
+    payload = request.args['signed_payload']
+    user_data = BigcommerceApi.oauth_verify_payload(payload, get_bc_client_secret())
+    if user_data is False:
+        return "Payload verification failed!", 401
+
+    bc_id = user_data['user']['id']
+    email = user_data['user']['email']
+    store_hash = user_data['store_hash']
+
+    app_user = get_query_first_result(AppUser, email)
+    if not app_user:
+        return 'You will need to re-install this app. Please uninstall and then go to marketplace and click install.'
+
+    # Log user in and redirect to app interface
+    session['storeuserid'] = app_user.email
+    return redirect(app.config['APP_URL'] + url_for('index'))
+
+
+# The Uninstall URL. See https://developer.bigcommerce.com/api/load
+@app.route('/bigcommerce/uninstall')
+def uninstall():
+    # Decode and verify payload
+    payload = request.args['signed_payload']
+    user_data = BigcommerceApi.oauth_verify_payload(payload, get_bc_client_secret())
+    if user_data is False:
+        return "Payload verification failed!", 401
+
+    bc_id = user_data['user']['id']
+    email = user_data['user']['email']
+    store_hash = user_data['store_hash']
+
+    app_user = get_query_first_result(AppUser, email)
+    if not app_user:
+        return 'Already uninstalled!', 204
+
+    app_user.delete()
+    return Response('Deleted', status=204)
+
+
+@app.route('/hsauth')
+def hsauth():
+    global signup_page
+    code = request.args.get('code')
+    redir_uri = get_hs_redir_uri()
+    client_id = get_hs_client_id()
+    client_secret = get_hs_client_secret()
+    try:
+        email = session['storeuserid']
+        app_user = get_query_first_result(AppUser, email)
+    except KeyError:
+        return "User not logged in! Please go back and click on app icon in admin panel.", 401
+
+    if code and app_user:
+        token_and_refresh = json.loads(exchange_code_for_token(code, client_id, client_secret, redir_uri))
+        print(token_and_refresh)
+        token_info = json.loads(get_token_info(token_and_refresh['access_token']))
+        print(token_info)
+        app_user.hs_refresh_token = token_and_refresh['refresh_token']
+        app_user.hs_access_token = token_and_refresh['access_token']
+        app_user.hs_expires_in = str(token_and_refresh['expires_in'])
+        app_user.hs_app_id = str(token_info['app_id'])
+        app_user.hs_hub_domain = token_info['hub_domain']
+        app_user.hs_hub_id = str(token_info['hub_id'])
+        app_user.hs_token_type = token_info['token_type']
+        app_user.hs_user = token_info['user']
+        app_user.hs_user_id = str(token_info['user_id'])
+        app_user.hs_scopes = token_info['scopes']
+
+        app_user.save()
+
+        signup_page = construct_chargebee_signup_url(session)
+    return render_template('success_hubspot.html', chargebee_hosted_url=signup_page)
+
+
+@app.route('/getstarted')
+def get_started():
+    hs_auth_url = construct_hubspot_auth_url()
+
+    return render_template('get_started.html', hs_auth_url=hs_auth_url)
+
+@app.route('/paymentsetup')
+def payment_setup():
+    signup_page = construct_chargebee_signup_url(session)
+
+    return render_template('chargebee_setup.html', chargebee_hosted_url=signup_page)
+
+
+@app.route('/paymentsuccess')
+def payment_success():
+
+    return render_template('success_chargebee.html')
+
+@app.route('/backtobigcommerce')
+def back_to_bigcommerce():
+    try:
+        email = session['storeuserid']
+        app_user = get_query_first_result(AppUser, email)
+    except KeyError:
+        return "User not logged in! Please go back and click on app icon in admin panel.", 401
+
+    redirect_url = 'https://store-{}.mybigcommerce.com/manage/'.format(app_user.bc_store_hash)
+    return redirect(redirect_url)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True, port=8100)
---
+++ hubspot_utils.py
@@ -0,0 +1,20 @@
+import requests
+
+HS_BASE_AUTH_URI = 'https://api.hubapi.com/oauth/v1/token'
+
+__all__ = ['exchange_code_for_token', 'get_token_info']
+
+
+def exchange_code_for_token(auth_code, clnt_id, clnt_secret, redir_uri):
+    headers = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'}
+    payload = 'grant_type=authorization_code&client_id={}&client_secret={}&redirect_uri={}&code={}'.format(clnt_id,
+                                                                                                           clnt_secret,
+                                                                                                           redir_uri,
+                                                                                                           auth_code)
+
+    return requests.post(HS_BASE_AUTH_URI, data=payload, headers=headers).text
+
+
+def get_token_info(token):
+    base_uri = 'https://api.hubapi.com/oauth/v1/access-tokens/'
+    return requests.get(base_uri + token).text
